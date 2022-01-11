ae3904c6aaa2f5b38b31b2fd087d4a4e007192a1
mazenbatrawi <71159005+mazenbatrawi@users.noreply.github.com>
2022-01-11 21:18:57 +0200

    Add files via upload

---
+++ Project/InsertProduct.java
@@ -0,0 +1,170 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+public class InsertProduct implements Initializable {
+
+    private String dbURL;
+    private String dbUsername = "root";
+    private String dbPassword = "16062001";
+    private String URL = "127.0.0.1";
+    private String port = "3306";
+    private String dbName = "fostoqa";
+    private Connection con;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    String query = null;
+
+
+    @FXML
+    private TextField barcode;
+
+    @FXML
+    private TextField selling_price;
+
+    @FXML
+    private TextField company_id;
+
+    @FXML
+    private TextField expire_date;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private TextField production_date;
+
+    @FXML
+    private TextField quantity;
+
+    @FXML
+    private TextField section_id;
+
+    @FXML
+    private AnchorPane table;
+
+    @FXML
+    private ImageView add_image;
+
+    @FXML
+    private ImageView back_image;
+
+    @FXML
+    void insert(ActionEvent event) throws SQLException, ClassNotFoundException {
+        if(barcode.getText().isEmpty() || company_id.getText().isEmpty() || expire_date.getText().isEmpty() ||
+            name.getText().isEmpty() || price.getText().isEmpty() || production_date.getText().isEmpty() ||
+            quantity.getText().isEmpty() || section_id.getText().isEmpty() || selling_price.getText().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill all fields!");
+            alert.showAndWait();
+        }
+        else if(!production_date.getText().isEmpty() && (production_date.getText().length() != 10 ||
+                (production_date.getText().length() == 10 &&
+                        (production_date.getText().charAt(2) != '-' || production_date.getText().charAt(5) != '-')))){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please Check The Production Date Format (dd-mm-yyyy)");
+            alert.showAndWait();
+        }
+        else if(!expire_date.getText().isEmpty() && (expire_date.getText().length() != 10 ||
+                (expire_date.getText().length() == 10 &&
+                        (expire_date.getText().charAt(2) != '-' || expire_date.getText().charAt(5) != '-')))){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please Check The Expire Date Format (dd-mm-yyyy)");
+            alert.showAndWait();
+        }
+        else{
+            String code = barcode.getText();
+            query = "select count(*) from product where barcode = '" + code + "'";
+            connectDB();
+
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            int cnt = 0;
+            while(resultSet.next()){
+                cnt = resultSet.getInt(1);
+            }
+            if(cnt != 0){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setContentText("There is a product with barcode " + code + " in the table");
+                alert.showAndWait();
+            }
+            else {
+                query = "insert into product values (" +
+                        "'" + barcode.getText() + "', " +
+                        "'" + name.getText() + "', " +
+                               company_id.getText() + ", " +
+                               section_id.getText() + ", " +
+                               price.getText() + ", " +
+                               quantity.getText() + ", " +
+                        "'" + production_date.getText() + "', " +
+                        "'" + expire_date.getText() + "', " +
+                               selling_price.getText() + ")";
+                Statement stmt = con.createStatement();
+                stmt.executeUpdate(query);
+
+                barcode.setText(null);
+                name.setText(null);
+                company_id.setText(null);
+                section_id.setText(null);
+                price.setText(null);
+                quantity.setText(null);
+                production_date.setText(null);
+                expire_date.setText(null);
+                selling_price.setText(null);
+
+                con.close();
+            }
+        }
+    }
+
+    @FXML
+    void back(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("sample_product.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Products");
+        stage.show();
+    }
+
+    private void connectDB() throws ClassNotFoundException, SQLException {
+        dbURL = "jdbc:mysql://" + URL + ":" + port + "/" + dbName + "?verifyServerCertificate=false";
+        Properties p = new Properties();
+        p.setProperty("user", dbUsername);
+        p.setProperty("password", dbPassword);
+        p.setProperty("useSSL", "false");
+        p.setProperty("autoReconnect", "true");
+        Class.forName("com.mysql.jdbc.Driver");
+        con = DriverManager.getConnection (dbURL, p);
+    }
+
+    @Override
+    public void initialize(java.net.URL url, ResourceBundle resourceBundle) {
+
+    }
+}
