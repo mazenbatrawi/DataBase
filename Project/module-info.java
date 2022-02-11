a2d24ddcf2168150ea69c28f758e2e656e945e35
mazenbatrawi <71159005+mazenbatrawi@users.noreply.github.com>
2022-02-11 15:02:40 +0200

    Delete Project directory

--- Project/module-info.java
+++
@@ -1,9 +0,0 @@
-module DataBaseProject {
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires java.sql;
-    requires mysql.connector.java;
-    requires javafx.web;
-
-    opens sample;
-}
